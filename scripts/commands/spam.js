e341ebbb855dccffb2e2579cd3da79e955df8547
Shakil33355 <skshakil33x@gmail.com>
2024-09-14 15:42:02 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["6156206173249"];
+  const permission = ["61564959257681"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin 🇸 🇰  🇸 🇭 🇦 🇰 🇮 🇱 (🇹 🇴 🇲 ) Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
